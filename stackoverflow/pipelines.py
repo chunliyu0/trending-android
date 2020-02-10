f1e7a38c5519568eec4ffb5dda8e576bbfdde059
chunli yu <ycl11761@gmail.com>
2020-02-10 09:44:17 -0800

    remove MongoDB for heroku deployment.

--- stackoverflow/pipelines.py
+++ stackoverflow/pipelines.py
@@ -4,32 +4,34 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-import pymongo
-from datetime import datetime,timedelta
-from dateutil.parser import parse
-from scrapy.exceptions import DropItem
 
-class StackoverflowPipeline(object):
-    def __init__(self):
-        self.connection = pymongo.MongoClient('127.0.0.1',27017)
-        self.db = self.connection.testdb1
-        self.collection = self.db.stackoverflow
 
-    def enough_days_collected(self,item):
-        asked_time = parse(item['asked_time'][0])
-        return (datetime.now(tz=asked_time.tzinfo) - asked_time) >= timedelta(minutes=30)
+# import pymongo
+# from datetime import datetime,timedelta
+# from dateutil.parser import parse
+# from scrapy.exceptions import DropItem
 
-    def process_item(self, item, spider):
-        if not self.connection or not item:
-            print('db not connected!')
-            return
+# class StackoverflowPipeline(object):
+#     def __init__(self):
+#         self.connection = pymongo.MongoClient('127.0.0.1',27017)
+#         self.db = self.connection.testdb1
+#         self.collection = self.db.stackoverflow
 
-        if self.enough_days_collected(item):
-            spider.crawler.engine.close_spider(spider,"enough items collected; stopping crawler...")
-        print('saving item to db...')
-        self.collection.save(item)
+#     def enough_days_collected(self,item):
+#         asked_time = parse(item['asked_time'][0])
+#         return (datetime.now(tz=asked_time.tzinfo) - asked_time) >= timedelta(minutes=30)
 
-    def __del__(self):
-        if self.connection:
-            self.connection.close()
+#     def process_item(self, item, spider):
+#         if not self.connection or not item:
+#             print('db not connected!')
+#             return
+
+#         if self.enough_days_collected(item):
+#             spider.crawler.engine.close_spider(spider,"enough items collected; stopping crawler...")
+#         print('saving item to db...')
+#         self.collection.save(item)
+
+#     def __del__(self):
+#         if self.connection:
+#             self.connection.close()
 
