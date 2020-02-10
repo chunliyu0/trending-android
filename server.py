4846c21c0ba0d98e9026c5a94b216197d9c698af
chunli yu <ycl11761@gmail.com>
2020-02-10 10:00:57 -0800

    update flask server.

--- server.py
+++ server.py
@@ -15,4 +15,5 @@ def index():
                            answer_html = answer_html)
 
 if __name__ =='__main__':
-    app.run(host='127.0.0.1',port=5000,debug=True)
+    app.run(threaded=True, port=5000)
+    # app.run(host='127.0.0.1',port=5000,debug=True)
