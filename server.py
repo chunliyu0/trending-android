eeaf81a1da1d4c0c2e719bb10da822ad3604194c
chunli yu <ycl11761@gmail.com>
2020-02-10 10:15:44 -0800

    no message

--- server.py
+++ server.py
@@ -15,5 +15,6 @@ def index():
                            answer_html = answer_html)
 
 if __name__ =='__main__':
-    app.run(threaded=True, port=5000)
+    app.run()
+    # app.run(threaded=True, port=5000)
     # app.run(host='127.0.0.1',port=5000,debug=True)
